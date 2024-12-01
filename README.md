c4fd4c91ffa54d687889fdb333f95d6d9c01b88d
Mackie Burgess <mackie.burgess@gmail.com>
2024-12-01 00:10:28 +0000

    Repo setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code 2024
+
+Solutions written in Rust, preferably with no external crates.
---
+++ src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    unreachable!("Run the command `cargo run --bin dayN`, where N is the day.");
+}
