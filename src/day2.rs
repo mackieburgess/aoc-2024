f4055e7982fcd6b2bab0981aff3e4d3c5ad2fc0e
Mackie Burgess <mackie.burgess@gmail.com>
2024-12-02 07:39:17 +0000

    day 2 - part 2

--- src/day2.rs
+++ src/day2.rs
@@ -1,24 +1,52 @@
 use std::fs;
 
-fn safe_reports() -> usize {
-    if let Some(input) = fs::read_to_string("data/2.input").ok() {
-        input.lines().map(|line| {
-            line.split_whitespace().filter_map(|c| c.parse::<usize>().ok()).collect::<Vec<_>>()
-        }).filter(|line| {
-            // Strictly increasing or strictly decreasing.
-            line.windows(2).all(|w| w[0] > w[1]) || line.windows(2).all(|w| w[0] < w[1])
-        }).filter(|line| {
-            // Each change is a small step.
-            line.windows(2).all(|w| w[0].abs_diff(w[1]) <= 3)
-        }).filter(|line| {
-            // Line isn't empty (as we rely heavily on `all`)
-            line.len() > 0
+fn split_into_numbers(input: &str) -> Vec<usize> {
+    // Take a line and split into whitespace separated numbers.
+    input.split_whitespace().filter_map(|c| c.parse().ok()).collect()
+}
+
+fn is_safe(report: &Vec<usize>) -> bool {
+    // Check:
+    // - Strictly increasing or strictly decreasing.
+    // - Each change is a small step.
+    // - Line isn't empty (as we rely heavily on `all`)
+    (report.windows(2).all(|w| w[0] > w[1]) || report.windows(2).all(|w| w[0] < w[1]))
+        && report.windows(2).all(|w| w[0].abs_diff(w[1]) <= 3)
+        && report.len() > 0
+}
+
+fn safe_reports(input: String) -> usize {
+    // Count reports which pass the `is_safe` check.
+    input
+        .lines()
+        .map(split_into_numbers)
+        .filter(|line| is_safe(line))
+        .count()
+}
+
+fn tolerated_reports(input: String) -> usize {
+    // Additionally, count reports which only fail because of one value.
+    input
+        .lines()
+        .map(split_into_numbers)
+        .filter(|line| {
+            if !is_safe(line) {
+                // Iterate through each element, try removing it and checking for safety.
+                (0..line.len()).any(|idx| {
+                    let mut new_line = line.clone();
+                    new_line.remove(idx);
+
+                    is_safe(&new_line)
+                })
+            } else {
+                return true;
+            }
         }).count()
-    } else {
-        panic!("No puzzle input")
-    }
 }
 
 fn main() {
-    println!("part one: {}", safe_reports())
+    if let Some(input) = fs::read_to_string("data/2.input").ok() {
+        println!("part one: {}", safe_reports(input.clone()));
+        println!("part two: {}", tolerated_reports(input));
+    }
 }
