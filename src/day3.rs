9c4ff3db98a3a1b3ff850876c545d2fb81c9c24c
Mackie Burgess <mackie.burgess@gmail.com>
2024-12-03 07:45:46 +0000

    day 3 - part 2

--- src/day3.rs
+++ src/day3.rs
@@ -1,23 +1,35 @@
 use std::fs;
 use regex::Regex;
 
-fn find_muls() -> usize {
-    if let Some(input) = fs::read_to_string("data/3.input").ok() {
-        let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+fn find_muls(input: &str) -> usize {
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
 
-        re.captures_iter(&input).filter_map(|capture| {
-            let (_, [mul1, mul2]) = capture.extract();
+    re.captures_iter(input).filter_map(|capture| {
+        let (_, [mul1, mul2]) = capture.extract();
 
-            match (mul1.parse::<usize>().ok(), mul2.parse::<usize>().ok()) {
-                (Some(m1), Some(m2)) => Some(m1 * m2),
-                _ => None
-            }
-        }).sum()
-    } else {
-        panic!("No puzzle input")
-    }
+        match (mul1.parse::<usize>().ok(), mul2.parse::<usize>().ok()) {
+            (Some(m1), Some(m2)) => Some(m1 * m2),
+            _ => None
+        }
+    }).sum()
+}
+
+fn annoying_muls(input: String) -> usize {
+    let re = Regex::new(r"don\'t\(\).*?do\(\)").unwrap();
+
+    // Remove all newlines, add `do()` at the end to fulfill any final capture.
+    let input = input.replace("\n", "") + "do()";
+
+    re.split(&input).map(|segment| {
+        find_muls(segment)
+    }).sum()
 }
 
 fn main() {
-    println!("part one: {}", find_muls());
+    if let Some(input) = fs::read_to_string("data/3.input").ok() {
+        println!("part one: {}", find_muls(&input));
+        println!("part two: {}", annoying_muls(input));
+    } else {
+        panic!("No puzzle input")
+    }
 }
