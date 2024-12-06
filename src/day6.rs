981cc4fbcf6716631442332604a436ffbe51ca13
Mackie Burgess <mackie.burgess@gmail.com>
2024-12-06 14:30:34 +0000

    day 6 - part 2

--- src/day6.rs
+++ src/day6.rs
@@ -2,6 +2,7 @@ use std::{collections::HashSet, fs};
 
 type PuzzleState = (usize, usize, (usize, usize), HashSet<(usize, usize)>, Direction);
 
+#[derive(Hash, PartialEq, Eq, Clone, Copy)]
 enum Direction {
     Up,
     Right,
@@ -64,23 +65,128 @@ fn parse_starting_puzzle_state(input: String) -> PuzzleState {
     );
 }
 
-fn steps_to_leave_map() -> usize {
-    if let Some(input) = fs::read_to_string("data/6.input").ok() {
-        let (
-            width,
-            height,
-            mut cursor,
-            walls,
-            mut direction
-        ) = parse_starting_puzzle_state(input);
+fn steps_to_leave_map(
+    width: usize,
+    height: usize,
+    mut cursor: (usize, usize),
+    walls: &HashSet<(usize, usize)>,
+    mut direction: Direction
+) -> Option<usize> {
+    let mut distinct_positions = HashSet::from([cursor]);
+    let mut directed_positions = HashSet::from([(cursor, direction)]);
 
-        let mut has_left_map = false;
-        let mut distinct_positions = HashSet::from([cursor]);
+    loop {
+        cursor = direction.step(cursor);
 
-        while !has_left_map {
-            cursor = direction.step(cursor);
+        if directed_positions.contains(&(cursor, direction)) {
+            // Infinite loop found.
+            return None;
+        }
 
-            distinct_positions.insert(cursor);
+        distinct_positions.insert(cursor);
+        directed_positions.insert((cursor, direction));
+
+        let going_off_map_edge = match direction {
+            Direction::Up => cursor.1 == 0,
+            Direction::Right => cursor.0 == width - 1,
+            Direction::Down => cursor.1 == height - 1,
+            Direction::Left => cursor.0 == 0
+        };
+
+        if going_off_map_edge {
+            break;
+        }
+
+        let place_for_wall = direction.step(cursor);
+
+        if walls.contains(&place_for_wall) {
+            direction.turn();
+
+            // Check that our turn hasn't put us on the verge of going off the map edge, or
+            // into another wall.
+            let going_off_map_edge = match direction {
+                Direction::Up => cursor.1 == 0,
+                Direction::Right => cursor.0 == width - 1,
+                Direction::Down => cursor.1 == height - 1,
+                Direction::Left => cursor.0 == 0
+            };
+
+            if going_off_map_edge {
+                break;
+            }
+
+            let place_for_wall = direction.step(cursor);
+
+            if walls.contains(&place_for_wall) {
+                direction.turn();
+            }
+        }
+    }
+
+    return Some(distinct_positions.len());
+}
+
+fn solve_map((width, height, cursor, walls, direction): PuzzleState) -> usize {
+    if let Some(number_of_steps) = steps_to_leave_map(width, height, cursor, &walls, direction) {
+        return number_of_steps;
+    } else {
+        panic!("Infinite loop found!");
+    }
+}
+
+fn time_loops((width, height, mut cursor, walls, mut direction): PuzzleState) -> usize {
+    let mut obstruction_spots = HashSet::new();
+
+    // We need to track positions to make sure we don't place walls where we've been.
+    let mut distinct_positions = HashSet::from([cursor]);
+
+    loop {
+        if !distinct_positions.contains(&direction.step(cursor)) {
+            let mut new_walls = walls.clone();
+            new_walls.insert(direction.step(cursor));
+
+            // New direction, to ensure we don't step through the obstruction.
+            let mut new_direction = direction.clone();
+            new_direction.turn();
+
+            // Account for a wall next to where you're standing.
+            if new_walls.contains(&new_direction.step(cursor)) {
+                new_direction.turn();
+            }
+
+            if steps_to_leave_map(
+                width,
+                height,
+                cursor,
+                &new_walls,
+                new_direction
+            ) == None {
+                obstruction_spots.insert(direction.step(cursor));
+            }
+        }
+
+        cursor = direction.step(cursor);
+
+        distinct_positions.insert(cursor);
+
+        let going_off_map_edge = match direction {
+            Direction::Up => cursor.1 == 0,
+            Direction::Right => cursor.0 == width - 1,
+            Direction::Down => cursor.1 == height - 1,
+            Direction::Left => cursor.0 == 0
+        };
+
+        if going_off_map_edge {
+            break;
+        }
+
+        let place_for_wall = direction.step(cursor);
+
+        if walls.contains(&place_for_wall) {
+            direction.turn();
+
+            // Check that our turn hasn't put us on the verge of going off the map edge, or
+            // into another wall.
 
             let going_off_map_edge = match direction {
                 Direction::Up => cursor.1 == 0,
@@ -90,46 +196,27 @@ fn steps_to_leave_map() -> usize {
             };
 
             if going_off_map_edge {
-                has_left_map = true;
-                continue;
+                break;
             }
 
             let place_for_wall = direction.step(cursor);
 
             if walls.contains(&place_for_wall) {
                 direction.turn();
-
-                // Check that our turn hasn't put us on the verge of going off the map edge, or
-                // into another wall.
-
-                let going_off_map_edge = match direction {
-                    Direction::Up => cursor.1 == 0,
-                    Direction::Right => cursor.0 == width - 1,
-                    Direction::Down => cursor.1 == height - 1,
-                    Direction::Left => cursor.0 == 0
-                };
-
-                if going_off_map_edge {
-                    has_left_map = true;
-                    continue;
-                }
-
-                let place_for_wall = direction.step(cursor);
-
-                if walls.contains(&place_for_wall) {
-                    direction.turn();
-                }
             }
-
-
         }
-
-        return distinct_positions.len();
-    } else {
-        panic!("No puzzle input")
     }
+
+    return obstruction_spots.len();
 }
 
 fn main() {
-    println!("part one: {}", steps_to_leave_map());
+    if let Some(input) = fs::read_to_string("data/6.input").ok() {
+        let puzzle_state = parse_starting_puzzle_state(input);
+
+        println!("part one: {}", solve_map(puzzle_state.clone()));
+        println!("part two: {}", time_loops(puzzle_state));
+    } else {
+        panic!("No puzzle input")
+    }
 }
