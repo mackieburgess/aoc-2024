4b3ef4d1f8a041414a3dea70a3567ff98e6a916a
Mackie Burgess <mackie.burgess@gmail.com>
2024-12-09 18:08:38 +0000

    day 8 - part 2

--- src/day8.rs
+++ src/day8.rs
@@ -3,7 +3,6 @@ use std::{
     fs,
 };
 
-#[allow(dead_code)]
 fn combinations<T: Clone>(obj: Vec<T>) -> Vec<(T, T)> {
     // [1, 2, 3] => [(1 2), (1 3), (2 1), (2 3), (3 1) (3 2)]
     (0..obj.len())
@@ -15,6 +14,10 @@ fn combinations<T: Clone>(obj: Vec<T>) -> Vec<(T, T)> {
         })
         .flatten()
         .collect()
+}
+
+fn is_in_range(antinode: &(isize, isize), width: isize, height: isize) -> bool {
+    antinode.0 >= 0 && antinode.1 >= 0 && antinode.0 < width && antinode.1 < height
 }
 
 fn parse_map(input: String) -> HashMap<char, Vec<(isize, isize)>> {
@@ -43,30 +46,57 @@ fn parse_map(input: String) -> HashMap<char, Vec<(isize, isize)>> {
     return towers;
 }
 
-fn antinodes_in_range() -> usize {
-    if let Some(input) = fs::read_to_string("data/8.input").ok() {
-        let width = input.clone().lines().next().unwrap().chars().count() as isize;
-        let height = input.clone().lines().count() as isize;
+fn antinodes(input: String) -> usize {
+    let width = input.clone().lines().next().unwrap().chars().count() as isize;
+    let height = input.clone().lines().count() as isize;
 
-        parse_map(input)
-            .into_values()
-            .map(|mesh| {
-                combinations(mesh)
-                    .iter()
-                    .map(|(fst, snd)| ((snd.0 + (snd.0 - fst.0)), (snd.1 + (snd.1 - fst.1))))
-                    .collect::<Vec<_>>()
-            })
-            .flatten()
-            .filter(|antinode: &(isize, isize)| {
-                antinode.0 >= 0 && antinode.1 >= 0 && antinode.0 < width && antinode.1 < height
-            })
-            .collect::<HashSet<_>>()
-            .len()
-    } else {
-        panic!("No puzzle input")
-    }
+    parse_map(input)
+        .into_values()
+        .map(|mesh| {
+            combinations(mesh)
+                .iter()
+                .map(|(fst, snd)| ((snd.0 * 2 - fst.0), (snd.1 * 2 - fst.1)))
+                .collect::<Vec<_>>()
+        })
+        .flatten()
+        .filter(|antinode| is_in_range(antinode, width, height))
+        .collect::<HashSet<_>>()
+        .len()
+}
+
+fn resonant_antinodes(input: String) -> usize {
+    let width = input.clone().lines().next().unwrap().chars().count() as isize;
+    let height = input.clone().lines().count() as isize;
+
+    parse_map(input)
+        .into_values()
+        .map(|mesh| {
+            combinations(mesh)
+                .iter()
+                .map(|(fst, mut snd)| {
+                    let mut resonancies = vec![];
+                    let difference = ((snd.0 - fst.0), (snd.1 - fst.1));
+
+                    while is_in_range(&snd, width, height) {
+                        resonancies.push(snd);
+                        snd = ((snd.0 + difference.0), (snd.1 + difference.1));
+                    }
+
+                    resonancies
+                })
+                .flatten()
+                .collect::<Vec<_>>()
+        })
+        .flatten()
+        .collect::<HashSet<_>>()
+        .len()
 }
 
 fn main() {
-    println!("part one: {}", antinodes_in_range());
+    if let Some(input) = fs::read_to_string("data/8.input").ok() {
+        println!("part one: {}", antinodes(input.clone()));
+        println!("part two: {}", resonant_antinodes(input));
+    } else {
+        panic!("No puzzle input")
+    }
 }
