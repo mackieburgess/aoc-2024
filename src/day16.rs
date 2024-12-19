66d664faade762acd66ab145b06c5eeb28a086e3
Mackie Burgess <mackie.burgess@gmail.com>
2024-12-19 22:29:42 +0000

    day 16 - part 2

--- src/day16.rs
+++ src/day16.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, fs};
+use std::{collections::{HashMap, HashSet}, fs};
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash)]
 enum Heading {
@@ -42,44 +42,74 @@ fn cheapest_route(
     heading: Heading,
     cursor: (usize, usize),
     end: (usize, usize)
-) -> usize {
-    let mut cache = HashSet::new();
-    let mut agenda = vec![(cursor, heading, 0)];
+) -> (usize, usize) {
+    let mut cache = HashMap::new();
+    let mut agenda = vec![(cursor, heading, 0, HashMap::new())];
 
-    while let Some((cursor, heading, score)) = agenda.pop() {
-        if cache.contains(&(cursor, heading)) {
+    let mut valid_paths: HashMap<((usize, usize), Heading), usize> = HashMap::new();
+    let mut cost = None;
+
+    while let Some((cursor, heading, score, mut path)) = agenda.pop() {
+        println!("{}", agenda.len());
+
+        if path.contains_key(&(cursor, heading)) {
             continue;
         }
 
-        cache.insert((cursor, heading));
+        if cache
+            .get(&(cursor, heading))
+            .is_some_and(|tally| *tally < score)
+        {
+            continue;
+        }
+
+        cache.insert((cursor, heading), score);
+        path.insert((cursor, heading), score);
 
         if cursor == end {
-            return score;
+            if cost == None {
+                cost = Some(score);
+            }
+            valid_paths.extend(path.iter());
+            continue;
         }
 
         let step = heading.step(&cursor);
 
         if !map[step.1][step.0] {
-            agenda.push((step, heading, score + 1));
+            agenda.push((step, heading, score + 1, path.clone()));
         }
 
         let left_rotate = heading.rotate_left();
         let right_rotate = heading.rotate_right();
 
         if !map[left_rotate.step(&cursor).1][left_rotate.step(&cursor).0] {
-            agenda.push((cursor, left_rotate, score + 1000));
+            agenda.push((cursor, left_rotate, score + 1000, path.clone()));
         }
         if !map[right_rotate.step(&cursor).1][right_rotate.step(&cursor).0] {
-            agenda.push((cursor, right_rotate, score + 1000));
+            agenda.push((cursor, right_rotate, score + 1000, path.clone()));
+        }
+
+        if let Some(cost) = cost {
+            agenda = agenda.into_iter().filter(|candidate| {
+                candidate.2 <= cost || !candidate.3.iter().any(|(key, tally)| {
+                    valid_paths.get(key).is_some_and(|ideal| ideal < tally)
+                })
+            }).collect();
         }
 
         agenda.sort_by(|a, b| b.2.cmp(&a.2));
     }
 
-    panic!("Agenda exhausted with no solution")
+    if let Some(found_cost) = cost {
+        (found_cost, valid_paths.into_iter().map(|((cursor, _heading), _score)| cursor).collect::<HashSet<_>>().len())
+    } else {
+        panic!("Agenda exhausted with no solution")
+    }
+
 }
 
-fn foo() -> usize {
+fn cheapest_route_cost() -> (usize, usize) {
     if let Some(input) = fs::read_to_string("data/16.input").ok() {
         let mut cursor = None;
         let mut end = None;
@@ -114,5 +144,7 @@ fn foo() -> usize {
 }
 
 fn main() {
-    println!("part one: {}", foo());
+    let (part_one_answer, part_two_answer) = cheapest_route_cost();
+    println!("part one: {}", part_one_answer);
+    println!("part two: {}", part_two_answer);
 }
